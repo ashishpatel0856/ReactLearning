cf2e9dabc4e03a33da426ac21a5569faecbf4bc8
Ashish Kumar <ashishkumarr0856@gmail.com>
2025-10-18 22:03:56 +0530

    registration

--- Registration-page/src/App.jsx
+++ Registration-page/src/App.jsx
@@ -1,16 +1,19 @@
-import { useState } from 'react'
-import Login from './components.jsx/Login'
-import Profile from './components.jsx/Profile'
-import Navbar from './components.jsx/Navbar'
+import { useState } from 'react';
+import Login from './components.jsx/Login';
+import Profile from './components.jsx/Profile';
+import Navbar from './components.jsx/Navbar';
 
 function App() {
-return (
-  <>
-  <Navbar />
-  <Login />
-  <Profile />
-  </>
-)   
+  return (
+    <>
+      <Navbar />
+      {/* Add margin-top to push content below navbar */}
+      <main className="mt-24 flex flex-col items-center justify-center min-h-screen bg-gray-50">
+        <Login />
+        <Profile />
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
