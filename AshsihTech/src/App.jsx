f9a4bac62cb2266bb6cec7b01cca9027ac9ca4f4
Ashish Kumar <ashishkumarr0856@gmail.com>
2025-10-20 11:27:08 +0530

    add

--- AshsihTech/src/App.jsx
+++ AshsihTech/src/App.jsx
@@ -1,12 +1,11 @@
-import { useState } from 'react'
-
+import { } from 'react'
+import Header from './components/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-     
+    <Header />
     </>
   )
 }
---
+++ AshsihTech/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='bg-[#2699fb] p-4'>
+  <h1 className=" text-2xl font-bold">Tailwind Working </h1>
+
+    </div>
+
+  )
+}
+
+export default Header
