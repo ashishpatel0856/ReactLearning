1366699a99f1bdd4efc20ab56f033c11c52509e2
Ashish Kumar <ashishkumarr0856@gmail.com>
2025-10-22 08:55:27 +0530

    tech

--- AshsihTech/src/App.jsx
+++ AshsihTech/src/App.jsx
@@ -2,6 +2,7 @@ import { } from 'react'
 import Header from './components/Header'
 import Banner from './components/Banner'
 import Experts from './components/Experts'
+import NewsLetter from './components/NewsLetter'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
     <Header />
     <Banner />
     <Experts />
+    <NewsLetter/>
     </>
   )
 }
--- AshsihTech/src/components/Experts.jsx
+++ AshsihTech/src/components/Experts.jsx
@@ -3,17 +3,17 @@ import laptop from '../assets/laptop.jpg'
 const Experts = () => {
     return (
         <div className='max-w-[1240px] mx-auto my-10
-    border-black grid grid-cols-3 '>
-            <div className='border border-[blue]  col-span-1'>
-                <img src={laptop} alt="" />
+     md:grid grid-cols-2 p-2 '>
+            <div className='  col-span-1 md:w-[80%]'>
+                <img src={laptop} alt="" className='inline' />
             </div>
 
-            <div className='border border-[blue]  col-span-2 flex flex-col'>
-                <h1 className='text-[#00df9a] font-bold my-2'> Learn From Experts</h1>
+            <div className='  cols-span-1 flex flex-col justify-center items-center'>
+                <h1 className=' text-[#00df9a] font-bold my-2'> Learn From Experts</h1>
                 <p className='my-2'>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis neque nesciunt odit in hic illum impedit obcaecati numquam, at nemo laboriosam earum, deserunt nam? Ratione quam ipsam officiis qui odio!
                 </p>
-                <button className='bg-black text-white rounded p-3'>Get Start</button>
+                <button className='w-[30%] bg-black text-white rounded p-3'>Get Start</button>
             </div>
         </div>
 
